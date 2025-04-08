4b1f406e863c2ac139c20f5b5b2102f4714f853d
SantiagoAndresDavid <sadavid@unicesar.edu.co>
2025-04-08 17:35:00 -0500

    delete test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent)
-      .toContain('Hello, mi-proyecto-no-stand-alone');
-  });
-
-
-  it(`should have as title 'mi-proyecto-no-stand-alone'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mi-proyecto-no-stand-alone');
-  });
 });
